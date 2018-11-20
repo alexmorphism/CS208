db71cd07da308c09467b8b9b4ac1100e54c05fe7
Castro <a_castro4@massbay.edu>
2018-11-20 10:58:36 -0500

    Computer lab, trying

---
+++ project4v2/src/Node.java
@@ -0,0 +1,16 @@
+
+public class Node {
+	
+	int key;
+	String value;
+	Node leftChildOne, rightChildOne, leftChildTwo, rightChildTwo;
+	
+	public Node(int key, String value) {
+		super();
+		this.key = key;
+		this.value = value;
+	}
+	
+	
+	
+}
